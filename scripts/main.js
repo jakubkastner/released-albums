93f32db9f48bb75db8a88b11b146c9f18ed0e041
Jakub Kastner <jakub.kastner@gmail.com>
2020-08-09 22:18:30 +0200

    fix redirect

--- scripts/main.js
+++ scripts/main.js
@@ -37,8 +37,8 @@
 
 var API_ID = 'd1c9a91ea65443af90946fde02fdda64';
 var API_SECRET = '26bbf4fad9384fd4bb3543649ade8b05';
-//var REDIRECT_URI = 'https://jakubkastner.github.io/released-albums/';
-var REDIRECT_URI = 'http://192.168.1.25:5500';
+var REDIRECT_URI = 'https://jakubkastner.github.io/released-albums/';
+//var REDIRECT_URI = 'http://192.168.1.25:5500';
 //var REDIRECT_URI = 'http://localhost:5500';
 //var REDIRECT_URI = 'http://127.0.0.1:5500';
 var STATE_KEY = 'spotify_auth_state';
