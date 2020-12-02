c32dd08d89749030c3f6bcb156d35f65df086c63
Jakub Kastner <jakub.kastner@gmail.com>
2020-12-02 13:11:26 +0100

    open in spotify now opens the app

--- scripts/show.js
+++ scripts/show.js
@@ -341,7 +341,7 @@ async function viewReleases(releaseType, year = 0, month = 0) {
                 elementReleaseDiv += `<i class="fas fa-plus album-playlist" title="Add to playlist" id="` + release.id + `_p"></i>`;
                 elementReleaseDiv += defaultPlaylistButton;
                 elementReleaseDiv += `<i class="fas fa-plus-square album-playlist-add-new" title="Add to new playlist '` + release.artistsString + ` - ` + release.name + `'" id="pd_` + release.id + `"></i>`;
-                elementReleaseDiv += `<a href="` + release.url + `" target="_blank" rel="noopener noreferrer"><i class="fab fa-spotify" title="Open in Spotify"></i></a>`;
+                elementReleaseDiv += `<a href="` + release.uri + ` rel="noopener noreferrer"><i class="fab fa-spotify" title="Open in Spotify"></i></a>`;
                 elementReleaseDiv += artistsYt;
                 elementReleaseDiv += `</div>
                             </div>
